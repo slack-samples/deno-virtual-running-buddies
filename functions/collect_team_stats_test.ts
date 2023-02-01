c40c3bef74d040f6d94f5ffdc1434c7efda0dde1
Ethan Zimbelman <ethan.zimbelman@slack-corp.com>
2023-01-31 17:40:31 -0800

    debug: log mock query filter

--- functions/collect_team_stats_test.ts
+++ functions/collect_team_stats_test.ts
@@ -30,7 +30,18 @@ mf.mock("POST@/api/apps.datastore.query", async (args) => {
     run.rundate >= dates[":start_date"] && run.rundate <= dates[":end_date"]
   ));
 
-  console.log("day=", new Date().getUTCDate());
+  mockRuns.forEach((run) => {
+    console.log(
+      run.rundate,
+      ">=",
+      dates[":start_date"],
+      "&&",
+      run.rundate,
+      "<=",
+      dates[":end_date"],
+    );
+  });
+
   console.log(runs);
 
   return new Response(JSON.stringify({ ok: true, items: runs }));
